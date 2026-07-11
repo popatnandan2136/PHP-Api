1b38bee97988da99e3d52b015b8768f6f241b2d0
NANDAN POPAT <popatnandan21@gmail.com>
2026-07-11 16:53:59 +0530

    commit

---
+++ nandan_products_api/index.php
@@ -0,0 +1,25 @@
+<!-- index.php: -->
+
+echo "<h2>Nandan API</h2>";
+<ul>
+    <!-- Connnect -->
+    <li><a href='nandan_products_api/connect.php'>Connect</a></li>
+
+    <!-- Sign Up and Sign In -->
+    <li><a href='nandan_products_api/signup.php'>Sign Up</a></li>
+	<li><a href='nandan_products_api/signin.php'>Sign In</a></li>
+
+    <!-- Product -->
+	<li><a href='nandan_products_api/insert_product.php'>Product Insert</a></li>
+	<li><a href='nandan_products_api/update_product.php'>Product Update</a></li>
+	<li><a href='nandan_products_api/delete_product.php'>Product Delete</a></li>
+	<li><a href='nandan_products_api/get_product.php'>Product Select</a></li>
+
+    <!-- Category -->
+	<li><a href='nandan_products_api/insert_category.php'>Category Insert</a></li>
+	<li><a href='nandan_products_api/update_category.php'>Category Update</a></li>
+	<li><a href='nandan_products_api/delete_category.php'>Category Delete</a></li>
+	<li><a href='nandan_products_api/get_category.php'>Category Select</a></li>
+
+</ul>
+</ul>
